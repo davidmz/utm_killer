e21b3acbe6547770f9257b2dd850558528ab6597
David Mzareulyan <david@hiero.ru>
2012-02-19 00:06:34 +0400

    Removing debug print:)

--- content-script.js
+++ content-script.js
@@ -3,7 +3,6 @@
     var l = location, q = l.search, utmRe = /[?&]utm_[a-z]+=[^&]+/;
 
     if (utmRe.test(q)) {
-        console.log("UTM OK");
         while (utmRe.test(q)) q = q.replace(utmRe, "");
         q = q.replace(/^[&?]+/, '');
 
